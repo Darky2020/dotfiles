85d78af7df78adb679cfa55764f394922fd6f778
Yaroslaw Biloshytskyi <yaroslaw0upwork@gmail.com>
2024-04-26 01:29:48 +0300

    Added some html injections to highlight inline css and javascript (PS: treesitter is amazing)

--- nvim/queries/html/injections.scm
+++ nvim/queries/html/injections.scm
@@ -5,3 +5,19 @@
 ((style_element
   (raw_text) @injection.content)
  (#set! injection.language "css"))
+
+(
+  (
+    (attribute_name) @constant (#match? @constant "onclick")
+    (quoted_attribute_value (attribute_value) @injection.content)
+  )
+ (#set! injection.language "javascript")
+)
+
+(
+  (
+    (attribute_name) @constant (#match? @constant "style")
+    (quoted_attribute_value (attribute_value) @injection.content)
+  )
+ (#set! injection.language "css")
+)
